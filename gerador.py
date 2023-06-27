62cda13ed201c9eb3cd29e14b1fad79872d40217
William Rafael Raymundo <72052122+WilliamRaymundo@users.noreply.github.com>
2023-06-26 23:27:06 -0300

    Feat: Criação de estrutura de pastas

--- gerador.py
+++ gerador.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from datetime import date
 import xml.etree.ElementTree as ET
+import os
 
 def gerar_xml():
-    autor = entry_autor.get()
+    desenvolvedor = entry_desenvolvedor.get() or "desenvolvedor"  # Obtém o texto do campo de entrada ou define como "desenvolvedor" se estiver vazio
     hoje = date.today().strftime("%Y-%m-%d")
     
     # Criação dos elementos XML
     raiz = ET.Element("root")
-    comentario = ET.Comment(" {}".format(autor))
+    comentario = ET.Comment(" {}".format(desenvolvedor))
     raiz.append(comentario)
     data = ET.SubElement(raiz, "data")
     data.text = hoje
@@ -19,8 +20,35 @@ def gerar_xml():
     # Gera a string XML
     xml_string = '<?xml version="1.0" encoding="utf-8"?>\n' + ET.tostring(raiz, encoding="utf-8").decode()
     
-    # Salva o XML em um arquivo
-    with open("output.xml", "w") as xml_file:
+    # Obtém o diretório atual onde o script está sendo executado
+    diretorio_atual = os.getcwd()
+    
+    # Define o caminho para a pasta RPA
+    caminho_rpa = os.path.join(diretorio_atual, "RPA")
+    
+    # Cria a pasta RPA se ela ainda não existir
+    if not os.path.exists(caminho_rpa):
+        os.mkdir(caminho_rpa)
+    
+    # Define o caminho para a pasta Documentos dentro de RPA
+    caminho_documentos = os.path.join(caminho_rpa, "Documentos")
+    
+    # Cria a pasta Documentos se ela ainda não existir
+    if not os.path.exists(caminho_documentos):
+        os.mkdir(caminho_documentos)
+    
+    # Define o caminho para a pasta Config dentro de RPA
+    caminho_config = os.path.join(caminho_rpa, "Config")
+    
+    # Cria a pasta Config se ela ainda não existir
+    if not os.path.exists(caminho_config):
+        os.mkdir(caminho_config)
+    
+    # Define o caminho completo para o arquivo XML de configuração
+    caminho_xml = os.path.join(caminho_config, "config.xml")
+    
+    # Salva o XML no caminho correto
+    with open(caminho_xml, "w") as xml_file:
         xml_file.write(xml_string)
     
     # Exibe uma mensagem de sucesso
@@ -31,11 +59,11 @@ janela = tk.Tk()
 janela.title("Gerador de XML")
 
 # Cria os elementos da interface
-lbl_autor = tk.Label(janela, text="Autor:")
-lbl_autor.pack()
+lbl_desenvolvedor = tk.Label(janela, text="Desenvolvedor:")
+lbl_desenvolvedor.pack()
 
-entry_autor = tk.Entry(janela)
-entry_autor.pack()
+entry_desenvolvedor = tk.Entry(janela)
+entry_desenvolvedor.pack()
 
 btn_ok = tk.Button(janela, text="OK", command=gerar_xml)
 btn_ok.pack()
